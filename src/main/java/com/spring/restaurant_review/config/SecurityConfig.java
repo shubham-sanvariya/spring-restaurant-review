94899a533db9d386c05794c436208c0911e462c3
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-05-03 23:52:37 +0530

    Add SecurityConfig class to configure Spring Security with OAuth2 resource server support, stateless session management, and CSRF protection disabled.

---
+++ src/main/java/com/spring/restaurant_review/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.spring.restaurant_review.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+            .authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated())
+            .oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(
+                    jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+                    ))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf(CsrfConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter();
+    }
+}
