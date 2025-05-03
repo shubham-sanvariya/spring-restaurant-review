cfb76213b8c9bd337c0b2ab0b2f0f5acb3905362
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-05-03 22:14:51 +0530

    Add Address entity with fields for street number, street name, unit, city, state, postal code, and country

---
+++ src/main/java/com/spring/restaurant_review/entities/Address.java
@@ -0,0 +1,37 @@
+package com.spring.restaurant_review.entities;
+
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Address {
+    
+    @Field(type = FieldType.Keyword)
+    private String streetNumber;
+
+    @Field(type = FieldType.Text)
+    private String streetName;
+
+    @Field(type = FieldType.Keyword)
+    private String unit;
+
+    @Field(type = FieldType.Keyword)
+    private String city;
+
+    @Field(type = FieldType.Keyword)
+    private String state;
+
+    @Field(type = FieldType.Keyword)
+    private String postalCode;
+
+    @Field(type = FieldType.Keyword)
+    private String country;
+}
