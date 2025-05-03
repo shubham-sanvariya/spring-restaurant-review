03bfcf8bad0b80d2fd15ba18d2901e2a6adfa29f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-05-03 22:18:24 +0530

    Add OperatingHours entity to manage weekly restaurant operating hours with nested TimeRange fields for each day

---
+++ src/main/java/com/spring/restaurant_review/entities/OperatingHours.java
@@ -0,0 +1,38 @@
+package com.spring.restaurant_review.entities;
+
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OperatingHours {
+    
+    @Field(type = FieldType.Nested)
+    private TimeRange monday;
+
+    @Field(type = FieldType.Nested)
+    private TimeRange tuesday;
+    
+    @Field(type = FieldType.Nested)
+    private TimeRange wednesday;
+    
+    @Field(type = FieldType.Nested)
+    private TimeRange thursday;
+    
+    @Field(type = FieldType.Nested)
+    private TimeRange friday;
+    
+    @Field(type = FieldType.Nested)
+    private TimeRange saturday;
+    
+    @Field(type = FieldType.Nested)
+    private TimeRange sunday;
+    
+}
