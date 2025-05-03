cea1dddd221249452247404feac747d53b4b5866
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-05-03 22:33:12 +0530

    Add Restaurant entity to represent restaurant details including name, cuisine type, contact information, average rating, geo-location, address, operating hours, photos, reviews, and the user who created it.

---
+++ src/main/java/com/spring/restaurant_review/entities/Restaurant.java
@@ -0,0 +1,60 @@
+package com.spring.restaurant_review.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.GeoPointField;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(indexName = "restaurants")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Restaurant {
+    
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Text)
+    private String cuisineType;
+
+    @Field(type = FieldType.Keyword)
+    private String contactInformation;
+
+    @Field(type = FieldType.Float)
+    private String averageRating;
+
+    @GeoPointField
+    private GeoPoint geoLocation;
+
+    @Field(type = FieldType.Nested)
+    private Address address;
+
+    @Field(type = FieldType.Nested)
+    private OperatingHours operatingHours;
+
+    @Field(type = FieldType.Nested)
+    @Builder.Default
+    private List<Photo> photos = new ArrayList<>();
+    
+    @Field(type = FieldType.Nested)
+    @Builder.Default
+    private List<Review> reviews = new ArrayList<>();
+
+    @Field(type = FieldType.Nested)
+    private User createdBy;
+
+}
