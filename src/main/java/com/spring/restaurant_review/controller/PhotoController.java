37015df6d5b43599de47f12ac7d6557078d97b32
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-05-04 21:35:56 +0530

    Add getPhoto endpoint to PhotoController for retrieving photos by ID, returning them as resources with appropriate content type and headers.

--- src/main/java/com/spring/restaurant_review/controller/PhotoController.java
+++ src/main/java/com/spring/restaurant_review/controller/PhotoController.java
@@ -1,5 +1,10 @@
 package com.spring.restaurant_review.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +16,9 @@ import com.spring.restaurant_review.entities.Photo;
 import com.spring.restaurant_review.mapper.PhotoMapper;
 import com.spring.restaurant_review.services.PhotoService;
 
+import java.net.http.HttpHeaders;
+
+import org.springframework.core.io.Resource;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -25,5 +33,19 @@ public class PhotoController {
     public PhotoDto uploadPhoto(@RequestParam("file") MultipartFile file) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id){
+       return photoService.getPhotoAsResource(id)
+            .map(photo -> ResponseEntity.ok().contentType(
+                MediaTypeFactory.getMediaType(photo)
+                    .orElse(MediaType.APPLICATION_OCTET_STREAM)
+            )
+            .header(org.springframework.http.HttpHeaders.CONTENT_DISPOSITION, "inline")
+            .body(photo)
+            )
+            .orElse(ResponseEntity.notFound().build());
+            
     }
 }
