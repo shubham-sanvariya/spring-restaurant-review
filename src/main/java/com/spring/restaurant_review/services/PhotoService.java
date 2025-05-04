c429c0abe74c96c69c2ffa0a2373e0e3f27a4f96
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-05-04 17:10:01 +0530

    Add getPhotoAsResource method to PhotoService interface for retrieving photos as resources.

--- src/main/java/com/spring/restaurant_review/services/PhotoService.java
+++ src/main/java/com/spring/restaurant_review/services/PhotoService.java
@@ -1,9 +1,14 @@
 package com.spring.restaurant_review.services;
 
+import java.util.Optional;
+
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.spring.restaurant_review.entities.Photo;
 
 public interface PhotoService {
     Photo uploadPhoto(MultipartFile file);
+
+    Optional<Resource> getPhotoAsResource(String id);
 }
